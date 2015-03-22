50e85e79e04c7f44a9b114c71ef4f7b4bbdf2bf4
Jeffry Gaston <gastoj3@gmail.com>
2015-03-22 20:56:08 +0100

    Beveled buttons

--- Source/DataExportView.cs
+++ Source/DataExportView.cs
@@ -17,7 +17,7 @@ namespace ActivityRecommendation.Source
             this.SetTitle("Export Data");
             
             HelpWindowBuilder helper = new HelpWindowBuilder();
-            helper.AddMessage("Warning: this txt file contains most of what you've entered into this application, and so it may become large");
+            helper.AddMessage("Warning: this txt file contains most of what you've entered into this application, and so it may become large.");
             helper.AddMessage("After you export this file and it opens in another program, it is probably easiest for you to simply save the file, close that program, and transfer it to a more powerful device, rather than trying to read it inside that program.");
             LayoutChoice_Set help = helper.Build();
 

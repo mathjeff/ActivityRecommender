547554b4eae509c2bf6d277c6f7d9bb55464e312
Jeffry Gaston <gastoj3@gmail.com>
2019-07-05 14:37:11 +0100

    restoring some utils

---
+++ util/copyData.sh
@@ -0,0 +1,9 @@
+cd $(dirname $0)
+set -e
+echo copying
+fileName="$(adb shell 'ls /storage/emulated/0/ActivityData-2*.txt' | sort | tail -n 1 | head -c 56)"
+destDir="ActivityData"
+cd
+cd ActivityData
+adb pull "$fileName" .
+echo copied "$fileName" to "~/$destDir/$(basename $fileName)"

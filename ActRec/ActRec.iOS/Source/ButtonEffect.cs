e6f66aa20cea5b668d8728ee693170b583393b84
Jeffry Gaston <gastoj3@gmail.com>
2020-03-08 19:34:45 +0100

    Making button texts do linebreaks on iOS

---
+++ ActRec/ActRec.iOS/Source/ButtonEffect.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ResolutionGroupName("VisiPlacement")]
+[assembly: ExportEffect(typeof(ActRec.iOS.ButtonEffect), "ButtonEffect")]
+namespace ActRec.iOS
+{
+    class ButtonEffect : PlatformEffect
+    {
+        public ButtonEffect()
+        {
+        }
+
+        protected override void OnAttached()
+        {
+            UIView view = this.Control;
+            UIButton button = view as UIButton;
+            button.LineBreakMode = UILineBreakMode.WordWrap;
+            button.TitleLabel.TextAlignment = UITextAlignment.Center;
+        }
+
+        protected override void OnDetached()
+        {
+        }
+
+    }
+}

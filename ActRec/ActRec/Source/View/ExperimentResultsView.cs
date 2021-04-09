e974463aa8702e53dadfab770b0e811fcbe6132a
Jeffry Gaston <gastoj3@gmail.com>
2021-04-09 22:01:31 +0100

    Wording improvements to the experiment completion screen
    to make it easier to read

--- ActRec/ActRec/Source/View/ExperimentResultsView.cs
+++ ActRec/ActRec/Source/View/ExperimentResultsView.cs
@@ -18,10 +18,8 @@ namespace ActivityRecommendation.View
             string earlierName = earlier.ActivityDescriptor.ActivityName;
             string laterName = later.ActivityDescriptor.ActivityName;
 
-            builder.AddMessage("You started an experiment at " + earlier.StartDate + "!");
-            builder.AddMessage("You agreed to several choices, including:");
+            builder.AddMessage("In your experiment at " + earlier.StartDate + ", you agreed to several choices, including:");
             builder.AddMessage("A: " + earlierName);
-            builder.AddMessage("and");
             builder.AddMessage("B: " + laterName);
 
             double difficultyRatio = experiment.Earlier.DifficultyEstimate.EstimatedSuccessesPerSecond / experiment.Later.DifficultyEstimate.EstimatedSuccessesPerSecond;
@@ -33,19 +31,20 @@ namespace ActivityRecommendation.View
             double laterMinutes = laterDuration.TotalMinutes;
 
             builder.AddMessage("Then, you spent " + this.round(earlierDuration.TotalMinutes) + "m on A");
-
-            builder.AddMessage("Later, you decided to do B at " + later.StartDate + ", which you spent " + this.round(laterDuration.TotalMinutes) + "m doing. ");
+            builder.AddMessage("Later, you spent " + this.round(laterDuration.TotalMinutes) + "m on B at " + later.StartDate);
 
             double earlierEffectiveness = earlier.RecomputedEfficiency.Mean * earlierMinutes;
             double laterEffectiveness = later.RecomputedEfficiency.Mean * laterMinutes;
 
             // technically, the two efficiencies get estimated separately, but that's complicated to explain and we think it might confuse the user if we try to explain it
-            builder.AddMessage("I estimated that your average efficiency on A would be about " + this.round(earlier.RecomputedEfficiency.Mean) +
-                ", making your effectiveness " + this.round(earlier.RecomputedEfficiency.Mean) + " * " + this.round(earlierMinutes) + "m = " + this.round(earlierEffectiveness) + "m on A.");
+            builder.AddMessage("I estimated that your efficiency on A would be about " + this.round(earlier.RecomputedEfficiency.Mean) +
+                ", making the difficulty of A be " + this.round(earlier.RecomputedEfficiency.Mean) + " * " + this.round(earlierMinutes) + "m = " + this.round(earlierEffectiveness) + "m");
 
-            builder.AddMessage("That also means that your total effectiveness later on B was about " + this.round(earlierEffectiveness) + "m * "
-                + this.round(difficultyRatio) + " = " + this.round(laterEffectiveness) + ", giving you an efficiency on B of " + this.round(laterEffectiveness) + " / " +
-                this.round(laterMinutes) + " = " + this.round(later.RecomputedEfficiency.Mean));
+            builder.AddMessage("So, the difficulty of B should be " + this.round(earlierEffectiveness) + "m * "
+                + this.round(difficultyRatio) + " = " + this.round(laterEffectiveness) + "m");
+
+            builder.AddMessage("And your efficiency on B should be " + this.round(laterEffectiveness) + "m / " +
+                this.round(laterMinutes) + "m = " + this.round(later.RecomputedEfficiency.Mean));
 
             builder.AddMessage("Isn't that interesting?");
 

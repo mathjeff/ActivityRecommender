6680a3de268b2bf2eeb1dc6fb771163d641cd5b7
Jeffry Gaston <gastoj3@gmail.com>
2021-05-01 20:48:18 +0100

    log viewer splitting text into multiple text blocks
    in case of text measurement rounding error

--- ActRec/ActRec/Source/View/LogViewer.cs
+++ ActRec/ActRec/Source/View/LogViewer.cs
@@ -23,8 +23,21 @@ namespace ActivityRecommendation.View
                     this.text = "Reading logs on this platform is not supported. Sorry";
                 else
                     this.text = textProvider.Get().ReadToEnd();
-                TextblockLayout textLayout = new TextblockLayout(this.text, 16, false, true);
-                this.SubLayout = ScrollLayout.New(textLayout);
+
+                Vertical_GridLayout_Builder builder = new Vertical_GridLayout_Builder();
+                // Split text into multiple text blocks so that if roudning error occurs in text measurement, the errors are spaced evenly.
+                // There could be 50 pages of text, and we don't want 2% rounding error to add a completely blank page at the end
+                List<string> lines = new List<string>(this.text.Split(new char[] { '\n' }));
+                int numLinesPerBlock = 10;
+                for (int i = 0; i < lines.Count; i += numLinesPerBlock)
+                {
+                    int maxIndex = Math.Min(i + numLinesPerBlock, lines.Count);
+                    List<string> currentLines = lines.GetRange(i, maxIndex - i);
+                    string currentText = string.Join("\n", currentLines);
+                    builder.AddLayout(new TextblockLayout(currentText, 16, false, true));
+                }
+
+                this.SubLayout = ScrollLayout.New(builder.BuildAnyLayout());
             }
             return base.GetBestLayout(query);
         }

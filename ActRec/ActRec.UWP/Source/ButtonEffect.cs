332edfa87b3219d29b95500da3e6fc77d355aba8
Jeffry Gaston <gastoj3@gmail.com>
2021-05-29 15:51:14 +0100

    Making the button padding closer to 0 on UWP

--- ActRec/ActRec.UWP/Source/ButtonEffect.cs
+++ ActRec/ActRec.UWP/Source/ButtonEffect.cs
@@ -16,7 +16,9 @@ namespace ActRec.UWP
         {
             Button button = this.Element as Button;
             button.BorderWidth = 0;
-            button.Padding = 0;
+            // We want padding of 0, but setting padding of 0 doesn't work (perhaps something overrides it later)
+            // If we set a padding of 1, though, that seems to work better, at least
+            button.Padding = 1;
             button.Margin = 0;
             //System.Diagnostics.Debug.WriteLine("Attaching " + this + " to element " + this.Element + " and control " + this.Control);
         }

f956c41b7b81f76dfc4d4980c9b3098747a086f3
Jeffry Gaston <gastoj3@gmail.com>
2022-10-27 13:39:02 +0100

    putData support to specify filepath

--- util/putData.sh
+++ util/putData.sh
@@ -3,6 +3,10 @@ set -e
 echo copying
 cd
 cd ActivityData
-fileName="$(ls ActivityData-*.txt | sort | tail -n 1 | head -c 56)"
+fileName="$1"
+if [ "$fileName" == "" ]; then
+  echo "Usage: $0 <filename>"
+  exit 1
+fi
 adb push "$fileName" /storage/emulated/0/ActivityRecommender
 echo copied "$fileName"
